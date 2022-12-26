9e497b0ef12574da675d03a363064061a97db500
lwh940824 <lwh940824@gmail.com>
2022-12-26 18:51:39 +0900

    라우터돔v6

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
-import React, { useState } from "react";
-import styled from "styled-components";
+import { Outlet } from "react-router";
 
-const Container = styled.div`
-  background-color: ${props => props.theme.bgColor};
-`;
-const H1 = styled.h1`
-  color: ${props => props.theme.textColor}
-`;
+import Router from "./Router";
 
 function App() {
-  
   return (
-    <Container>
-      <H1>proptected</H1>
-    </Container>
+      <>
+
+      </>
   );
 }
 

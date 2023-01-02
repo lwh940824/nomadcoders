e9eb09e0bddd04ea24de6fa9272f4055b5983234
whlee <whlee@pluxity.com>
2023-01-02 15:05:00 +0900

    framer-motion_Gestures

--- react-masterclass-animation/src/App.tsx
+++ react-masterclass-animation/src/App.tsx
@@ -1,42 +1,58 @@
 import styled from "styled-components";
 import { motion } from "framer-motion";
+import { useRef } from "react";
 
 const Wrapper = styled.div`
-    height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const BiggerBox = styled.div`
+  width: 600px;
+  height: 600px;
+  background-color: rgba(255, 255, 255, 0.34);
+  border-radius: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 const Box = styled(motion.div)`
-    width: 200px;
-    height: 200px;
-    grid-template-columns: repeat(2, 1fr);
-    background-color: rgba(255, 255, 255, 1);
-    border-radius: 40px;
-    box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.05);
+  width: 200px;
+  height: 200px;
+  grid-template-columns: repeat(2, 1fr);
+  background-color: rgba(255, 255, 255, 1);
+  border-radius: 40px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.05);
 `;
 
 const boxVariants = {
-    hover: { scale: 1.5, rotateZ: 90 },
-    click: { scale: 1, borderRadius: "100px" },
-    drag: {
-        backgroundColor: "rgb(46, 204, 113)",
-        transition: { duration: 10 },
-    },
+  hover: { scale: 1.5, rotateZ: 90 },
+  click: { scale: 1, borderRadius: "100px" },
+  drag: {
+    backgroundColor: "rgb(46, 204, 113)",
+    transition: { duration: 10 },
+  },
 };
 
 function App() {
-    return (
-        <Wrapper>
-            <Box
-                drag
-                variants={boxVariants}
-                whileHover={"hover"}
-                whileDrag="drag"
-                whileTap="click"
-            />
-        </Wrapper>
-    );
+  const biggerBoxRef = useRef<HTMLDivElement>(null);
+  return (
+    <Wrapper>
+      <BiggerBox ref={biggerBoxRef}>
+        <Box
+          drag
+          dragSnapToOrigin
+          dragElastic={0.5}
+          dragConstraints={biggerBoxRef}
+          variants={boxVariants}
+          whileHover={"hover"}
+          whileDrag="drag"
+          whileTap="click"
+        />
+      </BiggerBox>
+    </Wrapper>
+  );
 }
 
 export default App;
